0b3c4f66b90c4e343725aff8b7ab4da851de825a
Victor Pereira <victtor.vpb@gmail.com>
2017-05-10 22:18:53 -0300

    install express-ejs-layouts

--- ecommerce_front/src/configs/env/development.js
+++ ecommerce_front/src/configs/env/development.js
@@ -2,12 +2,16 @@ const path = require('path')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const expressEjsLayouts = require('express-ejs-layouts')
 
 module.exports =  (app) => {
     app.set('port', 9000)
     app.set('views', path.join(__dirname, './../../views'))
     app.set('view engine', 'ejs')
+    app.set('layout extractScripts', true)
+    app.set('layout extractStyles', true)
 
+    app.use(expressEjsLayouts)
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(morgan('dev'))
