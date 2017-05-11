d340e3bb00b812b4125edd3898b8813c1ba66538
Victor Pereira <victtor.vpb@gmail.com>
2017-05-10 22:11:38 -0300

    add gulp file

---
+++ ecommerce_front/gulpfile.js
@@ -0,0 +1,81 @@
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const nodemon = require('gulp-nodemon')
+const livereload = require('gulp-livereload')
+const notify = require('gulp-notify')
+const sass = require('gulp-sass')
+const plumber = require('gulp-plumber')
+const changed = require('gulp-changed')
+
+const path = {
+    htmlSrc: 'src/views',
+    sassSrc: 'public/scss',
+    jsSrc: 'public/js',
+
+    buildDir: 'build/',
+}
+
+let onError = (error) =>{
+    gutil.beep()
+    gutil.log(gutil.colors.red(error))
+}
+
+let initServer = () =>{
+    livereload.listen()
+    
+    nodemon({
+        script: 'app.js',
+        ext:'js'
+    })
+    .on('restart', () =>{
+        gulp.src('app.js')
+            .pipe(livereload())
+            pip(notify('Reloading... ;)'))   
+    })
+}
+
+gulp.task('build-html', () =>{
+    return gulp
+            .src(path.htmlSrc.concat('**/*.ejs'))
+            .pipe(gulp.dest(path.buildDir.concat('/views')))
+            .pipe(livereload())
+})
+
+gulp.task('build-css', () =>{
+    return gulp
+            .src(path.sassSrc.concat('**/*.scss'))
+            .pipe(sass({
+                includePaths: require('node-neat').includePaths,
+                style: 'nested',
+                onError: () => {
+                    console.log('Sass Error')
+                }
+            }))
+            .pipe(plumber({ errorHandler:onError }))
+            .pipe(gulp.dest(path.buildDir.concat('/css')))
+            .pipe(livereload())
+})
+
+gulp.task('build-js', () =>{
+    return gulp
+            .src(path.htmlSrc.concat('**/*.js'))
+            .pipe(plumber({ errorHandler:onError }))
+            .pipe(changed(path.buildDir.concat('/js')))
+            .pipe(gulp.dest(path.buildDir.concat('/js')))
+            .pipe(livereload())
+})
+
+gulp.task('build', ['build-html', 'build-js', 'build-css'],() =>{
+    return initServer()
+})
+gulp.task('watch', ()=>{
+    gulp.watch('src/views/**/*.ejs', ['build-html'])
+    gulp.watch('public/scss/**', ['build-css'])
+    gulp.watch('public/js/**.js', ['build-js'])
+})
+
+const env = process.env.NODE_ENV || 'development'
+
+if (env === 'development'){
+    return gulp.task('default', ['build', 'watch'])
+}
